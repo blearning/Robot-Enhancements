237dcb98868cedf3e3a22f80e8c755c90d89dfcb
briankasingli <brian.kasing.li@gmail.com>
2015-06-04 20:31:08 -0700

    completed enhancements #2

--- lib/laser.rb
+++ lib/laser.rb
@@ -1,6 +1,7 @@
 class Laser < Weapon
   def initialize
     super("Laser", 125, 25)
+    @range = 1
   end
 
 end
--- lib/robot.rb
+++ lib/robot.rb
@@ -1,5 +1,4 @@
 class Robot
-
     MAX_CAPACITY = 250
     MAX_HEALTH = 100
     MIN_HEALTH = 0
@@ -31,7 +30,8 @@ class Robot
   end
 
   def pick_up(item)
-    # binding.pry
+
+    item.feed(self) if item.is_a?(BoxOfBolts) && health <= 80
     if item.weight + items_weight <= MAX_CAPACITY
       @items << item
       (item.is_a? Weapon) ? @equipped_weapon = item : @equipped_weapon = nil
@@ -66,14 +66,33 @@ class Robot
   end
 
   def attack(target)
+
     attack!(target)
-    equipped_weapon == nil ? target.wound(5) : equipped_weapon.hit(target)
+    if enemy_in_range?(target)
+      equipped_weapon == nil ? target.wound(5) : equipped_weapon.hit(target)
+      @equipped_weapon = nil if equipped_weapon.is_a?(Grenade)
+    end
   end
 
   def attack!(target)
     begin 
       raise UnattackableEnemy, "INVALID - NOT ATTACKABLE: you can only attack robots" unless target.is_a? Robot
     end
+  end
+
+  def enemy_in_range?(target)
+    x_position_grenade_range = (position[0]-2..position[0]+2)
+    y_position_grenade_range = (position[1]-2..position[1]+2)
+
+    x_position_range = (position[0]-1..position[0]+1)
+    y_position_range = (position[1]-1..position[1]+1)
+
+    if equipped_weapon.is_a?(Grenade) && x_position_grenade_range.include?(target.position[0]) && y_position_grenade_range.include?(target.position[1])
+      return true
+    elsif (x_position_range.include?(target.position[0]) && y_position_range.include?(target.position[1]))
+      return true
+    end
+    return false
   end
 
 end
